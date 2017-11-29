2daf16a4e967f322ff6ff5ec072edce3ecaf734a
BaredJJ <1987BaredJJ@mail.ru>
2017-11-29 19:29:30 +0300

    Add class and methods connection to database for SQL server

---
+++ MyMusicBase/DataConnector/DataConnector.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+
+namespace DataConnector
+{
+    public static class DataConnector
+    {
+        //Создание экземпляра класса StringBuilder для подключения к БД
+        public static SqlConnectionStringBuilder SqlStringBuilder(string name, string pasword)
+        {
+            var connection = new SqlConnectionStringBuilder
+            {
+                DataSource = @".\SQLEXPRESS",
+                InitialCatalog = "MyMusic",
+                UserID = name,
+                Password = pasword,
+                Pooling = true
+            };
+            return connection;
+        }
+    }
+}
