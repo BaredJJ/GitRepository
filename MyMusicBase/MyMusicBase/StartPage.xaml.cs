b971aebc798464ee8e6598e9c98631a4cdd75d64
BaredJJ <1987BaredJJ@mail.ru>
2017-12-01 15:04:20 +0300

    Add the choice of variety serching. Upgrade the menu

--- MyMusicBase/MyMusicBase/StartPage.xaml.cs
+++ MyMusicBase/MyMusicBase/StartPage.xaml.cs
@@ -17,25 +17,87 @@ namespace MyMusicBase
 
         private void Search_OnClick(object sender, RoutedEventArgs e)
         {
-            string searchString = "SELECT * FROM Artist WHERE Name = '" + SearchBox.Text + "'";
-            StringBuilder str = new StringBuilder("Исполнитель: ");
-            int temp = DataConnector.GetId("ArtistId", searchString);
+            if (ChoiseOfSearch.SelectedIndex == 00)
+            {
+                AlbumText.Text = "";
+                ArtistText.Text = "";
+                StyleText.Text = "";
+                string searchString = "SELECT * FROM Artist WHERE Name = '" + SearchBox.Text + "'";
+                int temp = DataConnector.GetId("ArtistId", searchString);
+
+                GetArtistText(temp );
+                GetAlbumsText(temp);
+                GetStyleText(GetId(temp, "StyleId", "ArtistId"));
+            }
+            else if (ChoiseOfSearch.SelectedIndex == 1)
+            {
+                AlbumText.Text = "";
+                ArtistText.Text = "";
+                StyleText.Text = "";
+                string searchString = "SELECT * FROM Style WHERE Name ='" + SearchBox.Text + "'";
+                int temp = DataConnector.GetId("StyleId", searchString);
+
+                StyleText.Text = DataConnector
+                    .GetScalar(string.Format("SELECT Name FROM Style WHERE Name ='" + SearchBox.Text + "'")).ToString();
+
+                List<int> styleId = GetId(temp, "ArtistId", "StyleId");
+                foreach (var i in styleId)
+                {
+                    GetArtistText(i);
+                }
+                AlbumText.Text = "";
+            }
+            else if (ChoiseOfSearch.SelectedIndex == 2)
+            {
+                AlbumText.Text = "";
+                ArtistText.Text = "";
+                StyleText.Text = "";
+                string searchString = "SELECT * FROM Albums WHERE Name ='" + SearchBox.Text + "'";
+                int temp = DataConnector.GetId("ArtistId", searchString);
+
+                searchString = "SELECT Name FROM Albums WHERE Name ='" + SearchBox.Text + "'";
+                string str = DataConnector.GetScalar(searchString).ToString();
+                AlbumText.Text = str;
+
+                GetArtistText(temp);
+                GetStyleText(GetId(temp, "StyleId", "ArtistId"));
+            }
+ 
+        }
+
+        private void GetArtistText(int temp)
+        {
+            StringBuilder str = new StringBuilder( );
+            string searchString = "SELECT Name FROM Artist WHERE ArtistId = '" + temp + "'";
             str.AppendFormat(DataConnector.GetString(searchString));
-            ///////////////
-            str.AppendFormat("Альбомы: ");
-            searchString = "SELECT * FROM Albums WHERE ArtistId = '" + temp + "'";
+            ArtistText.Text = str.ToString( );
+        }
+
+        private void GetAlbumsText( int temp)
+        {
+            StringBuilder str = new StringBuilder( );
+            string searchString = "SELECT Name, DateRelease FROM Albums WHERE ArtistId = '" + temp + "'";
             str.AppendFormat(DataConnector.GetString(searchString));
-            ////////////////////////
-            str.AppendFormat("Жанры: ");
-            searchString = "SELECT StyleId FROM ArtistStyle WHERE ArtistId = '" + temp + "'";
+            AlbumText.Text = str.ToString( );
+        }
+
+        private static List<int> GetId(int temp, string chouse, string from)
+        {
+            StringBuilder str = new StringBuilder( );
+            string searchString = "SELECT " + chouse + " FROM ArtistStyle WHERE " + from + " = '" + temp + "'";
             List<int> styleId = DataConnector.GetList(searchString);
-            /////////////////////////////////////////
+            return styleId;
+        }
+
+        private void GetStyleText( List<int> styleId)
+        {
+            StringBuilder str = new StringBuilder();
             foreach (var i in styleId)
             {
-                searchString = "SELECT Style FROM Style WHERE StyleId = '" + i + "'";
-                str.AppendFormat(DataConnector.GetScalar(searchString).ToString());
+                string searchString = "SELECT Name FROM Style WHERE StyleId = '" + i + "'";
+                str.AppendFormat(DataConnector.GetScalar(searchString).ToString( ));
             }
-            Result.Text = str.ToString();
+            StyleText.Text = str.ToString( );
         }
     }
 }
